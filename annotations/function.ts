4e1b622ff14ba54261b7f44da2e136d9a027e2ec
Md Anas Sabah <mdanassabah@gmail.com>
2022-08-15 22:37:53 +0530

    type annotations in ts

---
+++ annotations/function.ts
@@ -0,0 +1,36 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwErr = (message: string): never => {
+  throw new Error(message);
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "rainy",
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
